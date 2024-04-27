056436c70e1c32d8f38a7b3f2d23de695ae759b9
HongYinhao <hongyh@ruc.edu.cn>
2024-04-27 15:53:44 +0800

    Modify deployment script to ensure logs are generated in the test folder

--- scripts/vcloud_deploy.sh
+++ scripts/vcloud_deploy.sh
@@ -12,15 +12,15 @@ for HOSTNAME in ${HOSTS}; do
     #SCRIPT="env SCHEMA_PATH=\"$2\" timeout -k 10m 10m gdb -batch -ex \"run\" -ex \"bt\" --args ./rundb -nid${count} >> results.out 2>&1 | grep -v ^\"No stack.\"$"
     # count < $NODE_CNT, run db
     if [ $count -lt $NODE_CNT ]; then
-        SCRIPT="env SCHEMA_PATH=\"$2\" timeout -k 9 3m ${PATHE}rundb -nid${count} > ${PATHE}dbresults.out 2>&1"
+        SCRIPT="cd ${PATHE};env SCHEMA_PATH=\"$2\" timeout -k 9 3m ${PATHE}rundb -nid${count} > ${PATHE}dbresults.out 2>&1"
         echo "${HOSTNAME}: rundb ${count}"
     # count >= $NODE_CNT & count < $NODE_CNT + $CL_NODE_CNT, run cl
     elif [ $count -lt `expr $NODE_CNT + $CL_NODE_CNT` ]; then
-        SCRIPT="env SCHEMA_PATH=\"$2\" timeout -k 9 3m ${PATHE}runcl -nid${count} > ${PATHE}clresults.out 2>&1"
+        SCRIPT="cd ${PATHE};env SCHEMA_PATH=\"$2\" timeout -k 9 3m ${PATHE}runcl -nid${count} > ${PATHE}clresults.out 2>&1"
         echo "${HOSTNAME}: runcl ${count}"
     # count >= $NODE_CNT + $CL_NODE_CNT, run st
     else
-        SCRIPT="env SCHEMA_PATH=\"$2\" timeout -k 9 3m ${PATHE}runst -nid${count} > ${PATHE}stresults.out 2>&1"
+        SCRIPT="cd ${PATHE};env SCHEMA_PATH=\"$2\" timeout -k 9 3m ${PATHE}runst -nid${count} > ${PATHE}stresults.out 2>&1"
         echo "${HOSTNAME}: runst ${count}"
     fi
     ssh -n -o BatchMode=yes -o StrictHostKeyChecking=no ${USERNAME}@${HOSTNAME} "${SCRIPT}" &
